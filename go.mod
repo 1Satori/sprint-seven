f852a8cfaac2061987576ad6d0d5d477ef4aad7c
satori <emogycs@gmail.com>
2024-03-17 20:41:28 +0300

    first

--- go.mod
+++ go.mod
@@ -1,1 +1,8 @@
-module sprint-seven
+module 1
+
+go 1.22rc1
+
+require (
+	github.com/go-chi/chi/v5 v5.0.12 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
@@ -46,13 +47,48 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte(answer))
 }
 
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := ... // здесь нужно создать запрос к сервису
+func TestMainHandlerWhenOK(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
 
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
 
+	if status := responseRecorder.Code; status == http.StatusOK && assert.NotEmpty(t, responseRecorder.Body) {
+	} else {
+		t.Errorf("status not ok")
+	}
+
 	// здесь нужно добавить необходимые проверки
+}
+
+func TestMainHandlerWhenCNT(t *testing.T) {
+	totalcnt := 4
+
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	countStr := req.URL.Query().Get("count")
+
+	if !assert.Equal(t, countStr, totalcnt) {
+		httptest.NewRequest("GET", "/cafe?count=4&city=moscow", nil)
+	}
+}
+
+func TestMainHandlerCity(t *testing.T) {
+	req := httptest.NewRequest("GET", "/cafe?count=5&city=moscow", nil)
+
+	responseRecorder := httptest.NewRecorder()
+	handler := http.HandlerFunc(mainHandle)
+	handler.ServeHTTP(responseRecorder, req)
+
+	cityStr := req.URL.Query().Get("city")
+
+	if !assert.Equal(t, cityStr, "moscow") {
+		t.Error("wrong city value")
+		responseRecorder.Code = http.StatusBadRequest
+	}
 }
